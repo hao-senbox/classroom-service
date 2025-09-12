37d0ecfa651d4c65d7de99a83781f0f114b07d04
provided <provided@provideds-Mac-mini.local>
2025-09-12 16:36:56 +0700

    push code

--- internal/leader/router.go
+++ internal/leader/router.go
@@ -7,9 +7,9 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine, handler *LeaderHandler) {
-	leaderGroup := r.Group("/api/v1/admin/classrooms/leader", middleware.Secured())
+	leaderGroup := r.Group("/api/v1/admin/classrooms", middleware.Secured())
 	{
-		leaderGroup.POST("", handler.AddLeader)
-		leaderGroup.POST("/remove", handler.DeleteLeader)
+		leaderGroup.POST("/leader", handler.AddLeader)
+		leaderGroup.POST("/remove/leader", handler.DeleteLeader)
 	}
 }
